5ac0a0b91089cadf50d503584f989c144270468d
rohit <rohit_pandey@persistent.com>
2021-05-27 12:27:30 +0530

    cross origin

--- src/main/java/com/rohit/controller/StudentController.java
+++ src/main/java/com/rohit/controller/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.rohit.Helper.Message;
 import com.rohit.entity.Student;
 import com.rohit.services.StudentServices;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/student")
 public class StudentController {
